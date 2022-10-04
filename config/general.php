bbeb7642cc9bf43168cfec55c17c64c40fc6aa96
Andreas Grether <webworker.ag@gmail.com>
2022-10-04 13:58:12 +0200

    Added fluid config

--- config/general.php
+++ config/general.php
@@ -1,53 +1,57 @@
-
 <?php
-/**
- * General Configuration
- *
- * All of your system's general configuration settings go in here. You can see a
- * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
- *
- * @see \craft\config\GeneralConfig
- */
 
+use craft\config\GeneralConfig;
 use craft\helpers\App;
 
 $isDev = App::env('CRAFT_ENVIRONMENT') === 'dev';
 $isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
 
-return [
-    'aliases' => [
-        '@web' => App::env('SITE_URL'),
-        '@webroot' => dirname(__DIR__) . '/web',
-    ],
-    'allowUpdates' => (bool)App::env('ALLOW_UPDATES'),
-    'allowAdminChanges' => (bool)App::env('ALLOW_ADMIN_CHANGES'),
-    'backupOnUpdate' => (bool)App::env('BACKUP_ON_UPDATE'),
-    'devMode' => (bool)App::env('DEV_MODE'),
-    'enableTemplateCaching' => (bool)App::env('ENABLE_TEMPLATE_CACHING'),
-    'isSystemLive' => (bool)App::env('IS_SYSTEM_LIVE'),
-    'resourceBasePath' => dirname(__DIR__) . '/web/cpresources',
-    'runQueueAutomatically' => (bool)App::env('RUN_QUEUE_AUTOMATICALLY'),
-    'testToEmailAddress' => App::env('TEST_MAIL') ?: null,
-    'enableGraphqlCaching' => (bool)App::env('ENABLE_GQL_CACHING'),
-    // Craft config settings from constants
-    'maxRevisions' => 5,
-    'cacheDuration' => false,
-    'defaultSearchTermOptions' => [
+$config = GeneralConfig::create()
+    ->devMode($isDev)
+    ->allowUpdates($isDev)
+    ->allowAdminChanges($isDev)
+    ->enableTemplateCaching(!$isDev)
+    ->enableGraphqlCaching(!$isDev)
+    ->defaultWeekStartDay(1)
+    ->isSystemLive(App::env('IS_SYSTEM_LIVE') ?: 0)
+    ->backupOnUpdate(1)
+    ->runQueueAutomatically(1)
+    ->cacheDuration(0)
+    ->maxRevisions(10)
+    ->defaultTokenDuration(86400)
+    ->enableCsrfProtection(1)
+    ->limitAutoSlugsToAscii(1)
+    ->convertFilenamesToAscii(true)
+    ->generateTransformsBeforePageLoad(true)
+    ->maxCachedCloudImageSize(3000)
+    ->maxUploadFileSize('100M')
+    ->omitScriptNameInUrls(true)
+    ->useEmailAsUsername(false)
+    ->usePathInfo(true)
+    ->preventUserEnumeration(true)
+    ->errorTemplatePrefix('errors/')
+    ->defaultSearchTermOptions([
         'subLeft' => true,
         'subRight' => true,
-    ],
-    'defaultWeekStartDay' => 1,
-    'defaultTokenDuration' => 86400,
-    'enableCsrfProtection' => true,
-    'errorTemplatePrefix' => '_errors/',
-    'limitAutoSlugsToAscii' => true,
-    'autosaveDrafts' => true,
-    'convertFilenamesToAscii' => true,
-    'generateTransformsBeforePageLoad' => true,
-    'maxCachedCloudImageSize' => 3000,
-    'maxUploadFileSize' => '100M',
-    'omitScriptNameInUrls' => true,
-    'useEmailAsUsername' => false,
-    'usePathInfo' => true,
-    'preventUserEnumeration' => true,
-];
+    ])
+    ->testToEmailAddress(App::env('TEST_MAIL') ?: null)
+    ->resourceBasePath(dirname(__DIR__) . '/web/cpresources')
+
+    ->aliases([
+        '@web' => App::env('SITE_URL'),
+        '@webroot' => dirname(__DIR__) . '/web',
+    ])
+    ->allowedFileExtensions(['jpg', 'png', 'jpeg', 'gif', 'svg', 'mp4', 'wov', 'mp3', 'wav', 'pdf', 'zip', 'csv', 'rar'])
+    ->omitScriptNameInUrls();
+
+if ($isProd) {
+    $config->disabledPlugins([
+        'elements-panel'
+    ]);
+}
+
+if (!$isProd) {
+    $config->disallowRobots();
+}
+
+return $config;
