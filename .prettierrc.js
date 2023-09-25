7cf12403b9a9fa9d68de57e9b44469b54007f198
Andreas Grether <webworker.ag@gmail.com>
2023-09-25 12:30:14 +0200

    Updates modules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  trailingComma: "all",
+  trailingComma: 'all',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
@@ -7,25 +7,25 @@ module.exports = {
   useTabs: false,
   overrides: [
     {
-      files: "*.json",
+      files: '*.json',
       options: {
         printWidth: 240,
       },
     },
   ],
-  plugins: [
-    'prettier-plugin-twig-melody',
-    ['prettier-plugin-tailwindcss', { resolveConfig: true }], // Use resolveConfig option
-  ],
-  tailwindConfig: "./tailwind.config.js",
+  //plugins: [
+  //  'prettier-plugin-twig-melody',
+  //  ['prettier-plugin-tailwindcss', { resolveConfig: true }], // Use resolveConfig option
+  //],
+  tailwindConfig: './tailwind.config.js',
   twigPrintWidth: 120,
   twigSingleQuote: false,
   twigMultiTags: [
-    "nav,endnav",
-    "minify,endminify",
-    "switch,case,default,endswitch",
-    "ifchildren,endifchildren",
-    "cache,endcache",
-    "js,endjs",
+    'nav,endnav',
+    'minify,endminify',
+    'switch,case,default,endswitch',
+    'ifchildren,endifchildren',
+    'cache,endcache',
+    'js,endjs',
   ],
 };
